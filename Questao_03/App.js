b2fbd6663c2ea3549d879869332852ee010c512d
ivanknd07 <ivanknd07@hotmail.com>
2022-04-17 12:31:27 -0300

    Adcionando Questao_03

---
+++ Questao_03/App.js
@@ -0,0 +1,24 @@
+import React, { Component, useState } from 'react';
+import { Container, Header, Content, Form, Label, Item, Text, StyleSheet, Input } from 'react-native';
+
+export default function App() {
+
+  const [nome, setNome] = useState("");
+
+  return (
+    <Container>
+        <Header />
+        <Content>
+          <Text>Qual o seu Nome? </Text>
+          <Form>
+            <Item Regular>
+              <Label />
+              <Input type="text" placeholder='Ex: Ivan' onChangeText={(text) => setNome(text)} />
+            </Item>
+          </Form>
+          <Text > Seja Bem vindo, {nome} !</Text>
+        </Content>
+      </Container>
+  );
+}
+
